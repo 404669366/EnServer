b7b5995b0218c562ae83c65d0d93a0a664439e11
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-05-21 12:49:21 +0800

    no message

--- appEn/startGlobalData.php
+++ appEn/startGlobalData.php
@@ -2,7 +2,7 @@
 require_once '../vendor/autoload.php';
 require_once '../vendor/autoloader.php';
 ini_set('memory_limit', '512M');
-$worker = new \vendor\globalData\server('127.0.0.1', 30001, 2);
+$worker = new \vendor\globalData\server('127.0.0.1', 30001);
 if (!defined('LINUX_START')) {
     \Workerman\Worker::runAll();
 }
