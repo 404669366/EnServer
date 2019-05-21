5ee313b401b198691267adcee013861cf3c018dc
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-05-21 12:44:27 +0800

    no message

---
+++ appEn/data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miku
+ * Date: 2019/5/21
+ * Time: 12:41
+ */
+require_once '../vendor/globalData/client.php';
+$global = new \vendor\globalData\client();
+var_dump($global->hGet('pileInfo','pile_10000'));
