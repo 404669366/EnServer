e5e39fab98559616e948538446584fb3bc9c1d67
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-09-30 20:50:22 +0800

    no message

--- handle/Web.php
+++ handle/Web.php
@@ -53,7 +53,6 @@ class Web
      */
     private static function beginCharge($client_id, $message)
     {
-        var_dump($message);
         if (Gateway::isUidOnline($message['pile'])) {
             $money = self::globalClient()->hGetField('UserInfo', $message['uid'], 'money') ?: 0;
             if ($money > 5) {
