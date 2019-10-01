f1e7fc3e65278a011bbc3b669d7beade10c88954
DESKTOP-OH3AP5P\40466 <404669366@qq.com>
2019-10-01 20:33:53 +0800

    no message

--- startup/Worker.php
+++ startup/Worker.php
@@ -6,6 +6,7 @@ $worker = new \GatewayWorker\BusinessWorker();
 $worker->name = 'worker';
 $worker->registerAddress = '127.0.0.1:20000';
 $worker->count = 2;
+$worker->eventHandler = 'startup/Events';
 if (!defined('LINUX_START')) {
     \Workerman\Worker::runAll();
 }
