aff38a9f45073fc2b04522dd31afa59a9e3af4b6
DESKTOP-OH3AP5P\40466 <404669366@qq.com>
2019-10-01 15:34:25 +0800

    no message

--- handle/TldPile.php
+++ handle/TldPile.php
@@ -65,7 +65,7 @@ class TldPile
     private static function cmd_104($client_id, $data)
     {
         if (in_array($data['gun'], [6, 7, 8])) {
-            var_dump(['g' => $data['gun'], 'w' => $data['workStatus'], 'l' => $data['linkStatus']]);
+            var_dump(['gun' => $data['gun'], 'workStatus' => $data['workStatus'], 'linkStatus' => $data['linkStatus']]);
         }
         Gateway::bindUid($client_id, $data['no']);
         $orderNo = isset($_SESSION['orderInfo'][$data['gun']]) ? $_SESSION['orderInfo'][$data['gun']] : '';
