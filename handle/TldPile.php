556fe9d103fec9161b117b987e93283ad1f52c7d
DESKTOP-OH3AP5P\40466 <404669366@qq.com>
2019-10-01 16:23:06 +0800

    no message

--- handle/TldPile.php
+++ handle/TldPile.php
@@ -167,7 +167,7 @@ class TldPile
 
     private static function cmd_202($client_id, $data)
     {
-        if (in_array($data['gun'], [7])) {
+        if (in_array($data['gun'], [6, 7, 8])) {
             echo '-----------------------------------------------------------';
             var_dump([
                 'cmd' => 202,
