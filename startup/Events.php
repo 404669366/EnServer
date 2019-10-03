ff39927d872b257314ced719c2cf3db44b2eb75e
DESKTOP-OH3AP5P\40466 <404669366@qq.com>
2019-10-03 15:58:03 +0800

    no message

--- startup/Events.php
+++ startup/Events.php
@@ -98,7 +98,7 @@ class Events
                         Gateway::bindUid($client_id, $data['no']);
                         $gun = self::globalClient()->hGet('GunInfo', $data['no'] . $data['gun']) ?: [];
                         if (isset($gun['orderNo'])) {
-                            if ($gun['workStatus'] == 1 && in_array($data['workStatus'], [0, 3, 4, 6])) {
+                            if ($gun['workStatus'] == 1 && in_array($data['workStatus'], [3, 4, 6])) {
                                 Gateway::sendToGroup($gun['orderNo'], json_encode(['code' => 200]));
                             }
                             if (in_array($gun['workStatus'], [0, 1, 2]) && $data['workStatus'] == 2) {
@@ -135,7 +135,7 @@ class Events
                                 }
                                 Gateway::sendToGroup($order['no'], json_encode(['code' => $code, 'data' => $order]));
                             }
-                            if ($gun['workStatus'] == 2 && in_array($data['workStatus'], [3, 6])) {
+                            if ($gun['workStatus'] == 2 && in_array($data['workStatus'], [0, 3, 6])) {
                                 $rule = self::getRule($data['no']);
                                 $order = self::globalClient()->hGet('ChargeOrder', $gun['orderNo']);
                                 $order['status'] = 2;
