36e3e1221cfe75d28527aa808aec2409bb950305
DESKTOP-OH3AP5P\40466 <404669366@qq.com>
2019-10-03 17:13:25 +0800

    no message

--- startup/Events.php
+++ startup/Events.php
@@ -92,8 +92,8 @@ class Events
                         Gateway::sendToClient($client_id, ['cmd' => 101, 'times' => $data['heartNo'] + 1]);
                         break;
                     case 104:
-                        if ($data['gun'] == 8) {
-                            var_dump(['gun' => $data['gun'], 'work' => $data['workStatus'], 'link' => $data['linkStatus']]);
+                        if ($data['linkStatus'] == 2) {
+                            echo $data['gun'] . '-' . $data['workStatus'] . PHP_EOL;
                         }
                         Gateway::bindUid($client_id, $data['no']);
                         $gun = self::globalClient()->hGet('GunInfo', $data['no'] . $data['gun']) ?: [];
