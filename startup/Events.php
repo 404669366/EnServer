502bc712a323e4eee3c61267a1a88a4e7133b59d
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-10-08 18:07:06 +0800

    no message

--- startup/Events.php
+++ startup/Events.php
@@ -138,11 +138,11 @@ class Events
                         Gateway::sendToClient($client_id, ['cmd' => 103, 'gun' => $data['gun']]);
                         break;
                     case 106:
-                        var_dump($data);
                         self::$db->query("REPLACE INTO en_pile(no,count,online) VALUES ('{$data['no']}','{$data['count']}',1)");
                         $_SESSION['no'] = $data['no'];
                         $_SESSION['count'] = $data['count'];
                         $_SESSION['rules'] = self::$db->select('rules')->from('en_pile')->where("no='{$data['no']}'")->column();
+                        var_dump($_SESSION['rules']);
                         $_SESSION['rules'] = json_decode($_SESSION['rules']['rules'], true);
                         Gateway::sendToClient($client_id, ['cmd' => 105, 'random' => $data['random']]);
                         Gateway::sendToClient($client_id, ['cmd' => 3, 'type' => 1, 'code' => 2, 'val' => self::getTime()]);
