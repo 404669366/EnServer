963bcf7283fd3e0d255bd726b12934c8d022ac05
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-11-05 14:15:33 +0800

    no message

--- startup/Events.php
+++ startup/Events.php
@@ -85,6 +85,9 @@ class Events
                         break;
                     case 104:
                         Gateway::bindUid($client_id, $data['no']);
+                        if ($data['workStatus'] == 6) {
+                            echo 'gun:' . $data['gun'] . '  err:' . $data['alarm'] . "\r\n";
+                        }
                         if ($data['workStatus'] == 2 && $data['linkStatus']) {
                             if ($order = self::$db->select('*')->from('en_order')->where("pile='{$data['no']}' AND gun='{$data['gun']}' AND status in(0,1)")->row()) {
                                 $rule = self::getRule($data['no']);
@@ -133,7 +136,6 @@ class Events
                             }
                         }
                         if ($data['workStatus'] == 6 && $data['linkStatus']) {
-                            echo 'gun:' . $data['gun'] . '  err:' . $data['alarm'] . "\r\n";
                             if ($order = self::$db->select('*')->from('en_order')->where("pile='{$data['no']}' AND gun='{$data['gun']}' AND status in(0,1)")->row()) {
                                 Gateway::sendToClient($client_id, ['cmd' => 5, 'gun' => $data['gun'], 'code' => 2, 'val' => 85]);
                                 $rule = self::getRule($data['no']);
